8d91539de70bd850b502c19ba6f6255463cbe39f
Dalma <dalma.saldarriaga@tsoftlatam.com>
2020-10-30 19:02:13 -0500

    Actualización buenas practicas

--- src/main/java/com/tsoft/bot/frontend/pages/pages/Altas_Residenciales.java
+++ src/main/java/com/tsoft/bot/frontend/pages/pages/Altas_Residenciales.java
@@ -1,5 +1,6 @@
 package com.tsoft.bot.frontend.pages.pages;
 
+import com.tsoft.bot.frontend.Base.BaseClass;
 import com.tsoft.bot.frontend.helpers.Hook;
 import com.tsoft.bot.frontend.pages.objects.Excel_MCSS;
 import com.tsoft.bot.frontend.pages.objects.Objects_MCSS;
@@ -9,42 +10,38 @@ import com.tsoft.bot.frontend.utility.GenerateWord;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.sikuli.script.ImagePath;
 import org.sikuli.script.Screen;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class Altas_Residenciales {
+public class Altas_Residenciales extends BaseClass {
 
-    Excel_MCSS excelMCSS = new Excel_MCSS();
-    Objects_MCSS ObjectsMCSS = new Objects_MCSS();
     public WebDriver driver;
     static GenerateWord generateWord = new GenerateWord();
 
     private List<HashMap<String, String>> getData() throws Throwable {
         return ExcelReader.data(Excel_MCSS.EXCEL_WEB, Excel_MCSS.POSTPAGO);
     }
-
-    public Altas_Residenciales() {
+    public Altas_Residenciales (WebDriver driver){
+        super(driver);
         this.driver = Hook.getDriver();
-    }
 
-    public void ingresamosALAURLMCSS(String casoDePrueba) throws Throwable {
+    }
+    public void IngresamosURL(String casoDePrueba) throws Throwable {
         try {
             int URL = Integer.parseInt(casoDePrueba) - 1;
-            String url = getData().get(URL).get(excelMCSS.COLUMNA_URL);
+            String url = getData().get(URL).get(Excel_MCSS.COLUMNA_URL);
             driver.get(url);
-            System.out.println(ImagePath.getBundlePath());
-            System.out.println(ImagePath.getBundlePath());
-            WebDriverWait wait = new WebDriverWait(driver, 60);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(ObjectsMCSS.RBTN_DEALER_USER));
-            if (driver.findElement(ObjectsMCSS.RBTN_DEALER_USER).isDisplayed()) {
-                driver.findElement(ObjectsMCSS.RBTN_DEALER_USER).click();
+            waitUntil(driver,60, Objects_MCSS.RBTN_DEALER_USER);
+            if (isDisplayed(driver, Objects_MCSS.RBTN_DEALER_USER)) {
+                click(driver,Objects_MCSS.RBTN_DEALER_USER);
+                ExtentReportUtil.INSTANCE.stepPass(driver, "Seleccionamos User dealer");
                 generateWord.sendText("Seleccionamos User dealer");
                 generateWord.addImageToWord(driver);
-                driver.findElement(ObjectsMCSS.LNK_DEALER_LOGIN).click();
-                wait.until(ExpectedConditions.visibilityOfElementLocated(ObjectsMCSS.TXT_USUARIO));
+                System.out.println("Seleccionamos Dealer User");
+                click(driver,Objects_MCSS.LNK_DEALER_LOGIN);
+                waitUntil(driver,60, Objects_MCSS.TXT_USUARIO);
             }
         } catch (Exception e) {
             ExcelReader.writeCellValue(Excel_MCSS.EXCEL_WEB, Excel_MCSS.POSTPAGO, 1, 19, "FAIL");
@@ -53,20 +50,21 @@ public class Altas_Residenciales {
             generateWord.addImageToWord(driver);
         }
     }
-
     public void IngresamosUsuario(String casoDePrueba) throws Throwable {
         try {
             int usuario = Integer.parseInt(casoDePrueba) - 1;
-            driver.findElement(ObjectsMCSS.TXT_USUARIO).clear();
+            driver.findElement(Objects_MCSS.TXT_USUARIO).clear();
             Thread.sleep(4000);
+            ExtentReportUtil.INSTANCE.stepPass(driver, "Login");
             generateWord.sendText("Login");
             generateWord.addImageToWord(driver);
-            String user = getData().get(usuario).get(excelMCSS.COLUMNA_USUARIO);
-            driver.findElement(ObjectsMCSS.TXT_USUARIO).sendKeys(user);
+            String user = getData().get(usuario).get(Excel_MCSS.COLUMNA_USUARIO);
+            driver.findElement(Objects_MCSS.TXT_USUARIO).sendKeys(user);
             Thread.sleep(2000);
             ExtentReportUtil.INSTANCE.stepPass(driver, "Ingresamos Usuario");
             generateWord.sendText("Ingresamos el usuario");
             generateWord.addImageToWord(driver);
+            System.out.println("Ingresamos usuario");
         }catch (Exception e){
             ExcelReader.writeCellValue(Excel_MCSS.EXCEL_WEB, Excel_MCSS.POSTPAGO, 1, 19, "FAIL");
             ExtentReportUtil.INSTANCE.stepFail(driver, "Fallo el caso de prueba : " + e.getMessage());
@@ -77,10 +75,10 @@ public class Altas_Residenciales {
     public void IngresamosContraseña(String casoDePrueba) throws Throwable {
 
         try {
-            int contraseña = Integer.parseInt(casoDePrueba) - 1;
-            driver.findElement(ObjectsMCSS.TXT_PASSWORD).clear();
-            String contra = getData().get(contraseña).get(excelMCSS.COLUMNA_CONTRASENIA);
-            driver.findElement(ObjectsMCSS.TXT_PASSWORD).sendKeys(contra);
+            int contrasenia = Integer.parseInt(casoDePrueba) - 1;
+            driver.findElement(Objects_MCSS.TXT_PASSWORD).clear();
+            String contra = getData().get(contrasenia).get(Excel_MCSS.COLUMNA_CONTRASENIA);
+            driver.findElement(Objects_MCSS.TXT_PASSWORD).sendKeys(contra);
             ExtentReportUtil.INSTANCE.stepPass(driver, "Ingresamos contraseña");
             generateWord.sendText("Ingresamos contraseña");
             generateWord.addImageToWord(driver);
@@ -93,9 +91,10 @@ public class Altas_Residenciales {
     }
     public void IngresoCorrectoalaPag() throws Throwable {
         try {
-            driver.findElement(ObjectsMCSS.BTN_INGRESAR).click();
+            driver.findElement(Objects_MCSS.BTN_INGRESAR).click();
+
             WebDriverWait wait = new WebDriverWait(driver, 60);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(ObjectsMCSS.TXT_DOCUMENTO));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(Objects_MCSS.TXT_DOCUMENTO));
             Thread.sleep(2000);
             generateWord.sendText("Ingresamos a MCSS");
             generateWord.addImageToWord(driver);
@@ -109,15 +108,15 @@ public class Altas_Residenciales {
     public void seleccionamosTipoDeAlta(String casoDePrueba) throws Throwable {
         try {
             WebDriverWait wait = new WebDriverWait(driver, 180);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(ObjectsMCSS.BTN_COMBO_POSTPAGO));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(Objects_MCSS.BTN_COMBO_POSTPAGO));
             int sim = Integer.parseInt(casoDePrueba) - 1;
-            String TIPO_SERVICIOs = getData().get(sim).get(excelMCSS.TIPO_SERVICIO);
+            String TIPO_SERVICIOs = getData().get(sim).get(Excel_MCSS.TIPO_SERVICIO);
             switch (TIPO_SERVICIOs.toUpperCase()){
                 case "POSTPAGO":
-                    driver.findElement(ObjectsMCSS.BTN_COMBO_POSTPAGO).click();
+                    driver.findElement(Objects_MCSS.BTN_COMBO_POSTPAGO).click();
                     break;
                 case "PREPAGO":
-                    driver.findElement(ObjectsMCSS.BTN_COMBO_PREPAGO).click();
+                    driver.findElement(Objects_MCSS.BTN_COMBO_PREPAGO).click();
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + TIPO_SERVICIOs);
--- src/main/java/com/tsoft/bot/frontend/steps/MCSS/AltasResidenciales.java
+++ src/main/java/com/tsoft/bot/frontend/steps/MCSS/AltasResidenciales.java
@@ -9,35 +9,25 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 
-public class AltasResidenciales {
+public class AltasResidenciales{
     public WebDriver driver;
-    Altas_Residenciales Altas = new Altas_Residenciales();
-    public AltasResidenciales() {
-        this.driver = Hook.getDriver();
-    }
-
+    Altas_Residenciales Altas = new Altas_Residenciales(driver);
     @Given("^INGRESAMOS A LA URL MCSS \"([^\"]*)\"$")
     public void ingresamosALAURLMCSS(String casoDePrueba) throws Throwable {
-        Altas.ingresamosALAURLMCSS(casoDePrueba);
+        Altas.IngresamosURL(casoDePrueba);
     }
-
     @When("^INGRESAMOS USUARIO A MCSS\"([^\"]*)\"$")
     public void ingresamosUSUARIOAMCSS(String casoDePrueba) throws Throwable {
         Altas.IngresamosUsuario(casoDePrueba);
-    }
 
+    }
     @And("^INGRESAMOS CONTRASEÑA A MCSS\"([^\"]*)\"$")
     public void ingresamosCONTRASEÑAAMCSS(String casoDePrueba) throws Throwable {
         Altas.IngresamosContraseña(casoDePrueba);
     }
-
     @Then("^INGRESO CORRECTO A LA PAGINA$")
     public void ingresoCORRECTOALAPAGINA() throws Throwable{
         Altas.IngresoCorrectoalaPag();
     }
 
-    @Given("^SELECCIONAMOS EL TIPO DE ALTA \"([^\"]*)\"$")
-    public void seleccionamosELTIPODEALTA(String arg0) throws Throwable {
-
-    }
 }
